d002d49a92c78c347f91b67ece5e13028017bb04
user <info@dominicschlegel.ch>
2015-03-17 21:11:49 +0100

    minor change to default route

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ScrumBoard' });
 });
 
 module.exports = router;
