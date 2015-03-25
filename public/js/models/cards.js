8f8379972c85c05dffec26358f72eb078d326c52
user <info@dominicschlegel.ch>
2015-03-25 15:49:06 +0100

    model now only saves given request attributes

--- public/js/models/cards.js
+++ public/js/models/cards.js
@@ -14,5 +14,20 @@ app.cards = Backbone.Model.extend({
     idAttribute: "id",
     urlRoot: '/card',
     
+    // i have this save method so that only the attributes given by http request are saved, instead of all attributes
+    save: function (attrs, options) { 
+    	attrs = attrs || this.toJSON();
+		options = options || {};
+
+		// If model defines serverAttrs, replace attrs with trimmed version
+		if (this.serverAttrs) attrs = _.pick(attrs, this.serverAttrs);
+
+		// Move attrs to options
+		options.attrs = attrs;
+
+		// Call super with attrs moved to options
+		Backbone.Model.prototype.save.call(this, attrs, options);
+	}
+    
     
 });
