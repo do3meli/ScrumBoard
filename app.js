8c91f40754b5e7c1141b8a8c38262abd52308f86
user <info@dominicschlegel.ch>
2015-03-25 14:23:37 +0100

    added body-parser npm

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const
   compression = require('compression'),		// create compression for http responses
   path = require('path'),			        		// we need path to get the dir name
   app = express(),						          	// make an express app
-  cards = require('./routes/card');		  	// define route for card
-  
+  cards = require('./routes/card'),		  	// define route for card
+  bodyParser = require('body-parser');		// needed to parse put/delete requests
 
 // set compression for app responses
 app.use(compression());
+
+// use body parser that is needed by the router
+app.use(bodyParser());
 
 // make the public folder accessible from root
 app.use(express.static(path.join(__dirname, 'public')));
